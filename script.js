ffe5c22cfeefa023e7f8374a53d35b34ed1f0493
JimenezPB <borja14vcf@gmail.com>
2020-11-05 00:18:28 +0100

    Funcion colores añadida

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function () { myFunction() };
+window.onload = setRandomColor();
 
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  console.log("hola")
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-    console.log(sticky)
-  } else {
-    navbar.classList.remove("sticky");
+function getRandomColor(){
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
   }
+  return color;
+}
+
+function setRandomColor(){
+  document.body.style.backgroundColor = getRandomColor();
 }
