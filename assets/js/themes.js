52c5c508cc68c27226b974c90899cbab5cab341f
borja <borja14vcf@gmail.com>
2024-03-26 18:13:49 +0100

    immedia experience and various

--- assets/js/themes.js
+++ assets/js/themes.js
@@ -1,57 +1,51 @@
-const defaultColor = "blue";
-const defaultStyle = "dark";
-const defaultTheme = "blue-theme-dark";
+const defaultColor = "blue"
+const defaultStyle = "dark"
+const defaultTheme = "blue-theme-dark"
 
 window.addEventListener("DOMContentLoaded", async () => {
-  const body = document.getElementById("body");
-  const userPreferredColor = localStorage.getItem("color") || defaultColor;
-  const userPreferredStyle = localStorage.getItem("style") || defaultStyle;
+    const body = document.getElementById("body");
 
-  changeStyle(userPreferredStyle);
+    const userPreferredColor = localStorage.getItem("color") || defaultColor;
+    const userPreferredStyle = localStorage.getItem("style") || defaultStyle;
 
-  const themes = await fetchThemes(userPreferredColor);
-  const className = getTheme(themes, userPreferredColor, userPreferredStyle);
-
-  body.className = className ?? defaultTheme;
+    await changeTheme(userPreferredStyle, userPreferredColor);
 });
 
-function getTheme(themes, color, style) {
-  return themes[color][style];
+function getTheme(themes, color, style){
+    return themes[color][style];
 }
 
 async function fetchThemes() {
-  const response = await fetch(`assets/themes/themes.json`);
-  return response.json();
+    const response = await fetch(`assets/themes/themes.json`);
+    return response.json();
 }
 
 function setColorPreference(color) {
-  localStorage.setItem("color", color);
+    localStorage.setItem("color", color);
+    location.reload();
 }
 
 function setStylePreference(style) {
-  localStorage.setItem("style", style);
-  changeStyle(style);
+    localStorage.setItem("style", style);
+    location.reload();
 }
 
 function changeStyle(userPreferredStyle) {
-  const sun = document.getElementById("sun-icon");
-  const moon = document.getElementById("moon-icon");
-
-  if (userPreferredStyle === "ligth") {
-    moon.className = "option";
-    sun.className = "hide-option";
-  } else {
-    sun.className = "option";
-    moon.className = "hide-option";
+    const sun = document.getElementById("sun-icon");
+    const moon = document.getElementById("moon-icon");
+  
+    if (userPreferredStyle === "ligth") {
+      moon.className = "option";
+      sun.className = "hide-option";
+    } else {
+      sun.className = "option";
+      moon.className = "hide-option";
+    }
   }
-}
 
-function showColorPalette() {
-  const palette = document.getElementById("color-palette");
+async function changeTheme(style, color){
+    const themes = await fetchThemes();
+    const className = getTheme(themes, color, style);
 
-  if (palette.className === "palette-hidden") {
-    palette.className = "palette";
-  } else {
-    palette.className = "palette-hidden";
-  }
+    body.className = className ?? defaultTheme
 }
